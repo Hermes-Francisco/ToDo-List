6b8f98936f62da1e7516ed6c486ccb1bf49fc5dd
Hermes Francisco <franciscohermes2000@hotmail.com>
2021-05-07 14:39:37 -0300

    i've removed the call for sequelize in the app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./sequelize');
 
 class App{
     constructor(){
@@ -15,7 +14,6 @@ class App{
 
     router(){
         this.app.use(routes);
-        this.sql = sequelize
     }
 }
 module.exports = new App().app;
