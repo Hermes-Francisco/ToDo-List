953c7afdd0f6ae0cd6b8305ebf9e2688681659e2
Hermes Francisco <franciscohermes2000@hotmail.com>
2021-05-11 00:14:41 -0300

    removi as bibliotecas de banco de dados

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 //Supported dialects: mssql, mariadb, mysql, postgres, and sqlite.
 module.exports = {
-    dialect: "mysql",
-    host: "127.0.0.1",
-    database: "todo",
-    username: "todo",
-    password: "todo"
+    dialect: "",
+    host: "",
+    database: "",
+    username: "",
+    password: ""
 };
